a0b2d36b3bce9a8afa3a17ebd495de19bea710cf
lalosalamanca1964 <142697060+lalosalamanca1964@users.noreply.github.com>
2023-12-12 07:35:03 -0500

    Update and rename z.js to server-list.js

---
+++ server-list.js
@@ -0,0 +1,23 @@
+const JBTSinfo = {
+    gif:"IMG_8198.GIF",
+    name:"Jailbreak Trading Shifters",
+    infoName:"JBTS",
+    membercount:"1276",
+    focus:"Trading",
+    serverImage:"IMG_7798.jpeg",
+    serverLink:"discord.gg/jbv"
+}
+
+document.getElementById("MemCountFSL").innerHTML = JBTSinfo.membercount; 
+
+const JBVinfo = {
+    gif:"jbvalues_banner.gif",
+    name:"JB Values (JBV)",
+    infoName:"JBV",
+    membercount:"2257",
+    focus:"Values",
+    serverImage:"IMG_7798.jpeg",
+    serverLink:"discord.gg/jbv"
+}
+
+document.getElementById("JBVMemCountFSL").innerHTML = JBVinfo.membercount; 
